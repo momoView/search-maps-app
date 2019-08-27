f05cd520cd5d4b3b3910a285367fa01bd1525eb3
momoView <momochanmo.zz@gmail.com>
2019-08-27 19:36:54 +0100

    feat(maps-autocomplete): added needed actions
    & effects, added autocomplete input for places & added all necessary packages
    
    feat in branch #feat/submodule-maps-autocomplete

--- src/app/search-maps/search-maps.module.ts
+++ src/app/search-maps/search-maps.module.ts
@@ -1,19 +1,30 @@
 import { NgModule } from '@angular/core';
 import { AgmCoreModule } from '@agm/core';
+import { GooglePlaceModule } from "ngx-google-places-autocomplete";
 
+import { SharedModule } from '../shared/shared.module'
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { environment } from '../../environments/environment'
+
+import { MapsAutocompleteComponent } from
+  './maps-autocomplete/maps-autocomplete.component';
 import { SearchMapsComponent } from './search-maps.component';
 import { MapsComponent } from './maps/maps.component';
-import { SharedModule } from '../shared/shared.module'
 
 @NgModule({
   declarations: [
     SearchMapsComponent,
     MapsComponent,
+    MapsAutocompleteComponent,
   ],
   imports: [
     SharedModule,
     AgmCoreModule.forRoot({apiKey:
       'AIzaSyBMSGEgUzZDexreOUINhcn9ko1j5bHqdNw'}),
-  ]
+    GooglePlaceModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+  ],
 })
 export class SearchMapsModule {}
