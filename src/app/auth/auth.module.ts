84b6207a038c9201c92415ea8f7cda801183b2cd
momoView <momochanmo.zz@gmail.com>
2019-12-08 00:43:36 +0100

    feat(auth): added profile & change password
    components & added them to auth module & added profile section in header
    
    belongs to #feat/auth-module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,6 +3,8 @@ import { FormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ChangePasswordComponent } from './profile/change-password/change-password.component';
+import { ProfileComponent } from './profile/profile.component';
 import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
 
@@ -10,7 +12,9 @@ import { SigninComponent } from './signin/signin.component';
   declarations:[
     SignupComponent,
     SigninComponent,
+    ChangePasswordComponent,
     ForgotPasswordComponent,
+    ProfileComponent
   ],
   imports:[
     FormsModule,
