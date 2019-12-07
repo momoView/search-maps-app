98a0e509f0ec70d05967317ca399e4e5c2811072
momoView <momochanmo.zz@gmail.com>
2019-12-07 23:21:48 +0100

    feat(auth): added auth interceptor for every http
    request to /places api to add token to requests
    
    belongs to #feat/auth-module

---
+++ src/app/shared/auth.interceptor.ts
@@ -0,0 +1,25 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { take, switchMap } from 'rxjs/operators';
+
+import * as fromSM from '../search-maps/store/search-maps.reducers';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private store: Store<fromSM.FeatureState>) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (req.url === "https://proud-limiter-210411.firebaseio.com/places.json") {
+      return this.store.select('auth').pipe(take(1),switchMap(
+        (authState) => {
+          const copiedReq = req.clone({ params: req.params.set('auth', authState.token) });
+          return (next.handle(copiedReq));
+        }
+      ));
+    } else {
+      return (next.handle(req));
+    }
+  }
+}
